274d302421a28528c395681b77206cae51f1982b
Jens van den Hurk <jensvandenhurk@outlook.com>
2026-05-19 19:46:39 +0200

    JSONParser2PDFGenerator For SMM Testen gemaakt.

---
+++ parsertest/teststraat/FunctionalTest.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+public static class JsonParserService
+{
+    public static DiscordExport? DeserializeDiscordExport(string rawJson)
+    {
+        return JsonConvert.DeserializeObject<DiscordExport>(rawJson);
+    }
+
+    public static bool HasMessages(DiscordExport? export)
+    {
+        return export != null && export.Messages.Count > 0;
+    }
+
+    public static string GetAuthorName(DiscordMessage message)
+    {
+        return message.Author?.Nickname
+               ?? message.Author?.Name
+               ?? "Onbekende auteur";
+    }
+
+    public static string GetContent(DiscordMessage message)
+    {
+        return string.IsNullOrWhiteSpace(message.Content)
+            ? "[Geen tekst]"
+            : message.Content;
+    }
+}
